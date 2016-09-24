a9e41bf55e4635e0822f9f93933b5f7af9550504
luigineto <luigineto@hotmail.com>
2016-09-24 18:34:37 +0200

    add scripts.js JAVASCRIPT file and JQuery

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$("h2").click(function() {
+	alert(userSentence);
+});
+
+$("h3").click(function() {
+	alert(cipherSentence);
+})
+
+
+var runCipherFunctions = function() {
+	captalizeLetters(userSentence);
+	reverseLetters(cipherSentence);
+	cipherSentence = userSentence + cipherSentence;
+	eighthLetter(userSentence);
+	cipherSentence = cipherSentence.split('').reverse().join('')
+	alert(cipherSentence);
+};
+
+var captalizeLetters = function(sentence) {
+	return cipherSentence = sentence.slice(0, 1).toUpperCase() + sentence.slice(-1).toUpperCase();
+};
+
+var reverseLetters = function(sentence) {
+	return cipherSentence = cipherSentence.split('').reverse().join('');
+};
+
+var eighthLetter = function(sentence) {
+	cipherSentence = sentence.charAt(sentence.length / 2) + cipherSentence;
+};
+
+var cipherSentence = "";
+
+var userSentence = prompt("Enter your sencence:");
+
+runCipherFunctions();
